a612267fb9ea8761c1a4db1825ee2fbd9566d42a
Filatov Anton <filatov@intaro.ru>
2025-08-15 10:46:36 +0300

    start just from scratch

--- tests/bootstrap.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-error_reporting(E_ALL & ~E_DEPRECATED & ~E_USER_DEPRECATED);
-
-require dirname(__DIR__).'/vendor/autoload.php';
-
-if (file_exists(dirname(__DIR__).'/config/bootstrap.php')) {
-    require dirname(__DIR__).'/config/bootstrap.php';
-}
